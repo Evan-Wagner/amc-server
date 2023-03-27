af73b602b0b7b203a9d73ba5e878186dd9d3948c
Evan Wagner <evanwagner@Ol-Reliable.local>
2023-03-27 16:34:20 -0400

    deployed to vercel

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import db from './dbs/tune-db.js';
 import tuneRouter from './routes/tune-router.js';
 
+console.log('test');
+
 const app = express();
 const apiPort = 3000;
 
